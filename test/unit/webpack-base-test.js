6d07f564099dec44162a0e69ff953416749b7ce4
shangyunsiwork <shangyunsiwork@163.com>
2020-08-20 11:31:16 +0800

    test: update

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,6 +4,6 @@ describe('webpack.base.js', () => {
   const baseConfig = require('../../lib/webpack.base');
 
   it('entry',()=> {
-    assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1);
+    assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1 , true);
   })
 })
